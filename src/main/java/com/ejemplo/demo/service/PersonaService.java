4cb20050c3e36057ad48749c9bb3400b74bbe080
Enrique Sanchez <enrique.sanchez@autozone.com>
2021-11-24 16:30:09 -0700

    Agregar buscar persona en Servicio

--- src/main/java/com/ejemplo/demo/service/PersonaService.java
+++ src/main/java/com/ejemplo/demo/service/PersonaService.java
@@ -1,10 +1,13 @@
 package com.ejemplo.demo.service;
 
+import com.ejemplo.demo.exception.PersonaNoEncontradaException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ejemplo.demo.entity.Persona;
 import com.ejemplo.demo.repository.PersonaRepository;
+
+import java.util.Optional;
 
 @Service
 public class PersonaService {
@@ -13,5 +16,15 @@ public class PersonaService {
 
     public Persona agregarPersona(final Persona personaNueva) {
         return repositorio.save(personaNueva);
+    }
+
+    public Persona buscarPersona(final long personaId) throws PersonaNoEncontradaException {
+        Optional<Persona> personaOpcional = repositorio.findById(personaId);
+
+        if (personaOpcional.isPresent()) {
+            return personaOpcional.get();
+        } else {
+            throw new PersonaNoEncontradaException();
+        }
     }
 }
