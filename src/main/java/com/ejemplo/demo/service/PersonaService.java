441c86ca18e3783e3a00016a6c9003ab16893ed0
Enrique Sanchez <enrique.sanchez@autozone.com>
2021-11-24 15:54:50 -0700

    Agregado agretar persona en Servicio

--- src/main/java/com/ejemplo/demo/service/PersonaService.java
+++ src/main/java/com/ejemplo/demo/service/PersonaService.java
@@ -1,4 +1,17 @@
 package com.ejemplo.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ejemplo.demo.entity.Persona;
+import com.ejemplo.demo.repository.PersonaRepository;
+
+@Service
 public class PersonaService {
+    @Autowired
+    PersonaRepository repositorio;
+
+    public Persona agregarPersona(final Persona personaNueva) {
+        return repositorio.save(personaNueva);
+    }
 }
