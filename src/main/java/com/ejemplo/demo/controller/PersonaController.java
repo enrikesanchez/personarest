8ca4463336d9258f23f945c722eb3d4a7093558c
Enrique Sanchez <enrique.sanchez@autozone.com>
2021-11-24 16:14:06 -0700

    Agregar agregar persona controller

--- src/main/java/com/ejemplo/demo/controller/PersonaController.java
+++ src/main/java/com/ejemplo/demo/controller/PersonaController.java
@@ -1,4 +1,22 @@
 package com.ejemplo.demo.controller;
 
+import com.ejemplo.demo.entity.Persona;
+import com.ejemplo.demo.service.PersonaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PersonaController {
+    @Autowired
+    PersonaService servicio;
+
+    @PostMapping("/personas")
+    public ResponseEntity<Persona> agregarPersona(@RequestBody final Persona personaNueva) {
+        Persona personaGuardada = servicio.agregarPersona(personaNueva);
+        return new ResponseEntity<>(personaGuardada, HttpStatus.CREATED);
+    }
 }
