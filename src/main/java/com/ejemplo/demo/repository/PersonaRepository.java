3fe00cf39eca3141dcc7b13b2b89065d4cfac038
Enrique Sanchez <enrique.sanchez@autozone.com>
2021-11-24 15:26:28 -0700

    Agregados Entidad y Repositorio de Persona

--- src/main/java/com/ejemplo/demo/repository/PersonaRepository.java
+++ src/main/java/com/ejemplo/demo/repository/PersonaRepository.java
@@ -1,5 +1,11 @@
 package com.ejemplo.demo.repository;
 
-public interface PersonaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ejemplo.demo.entity.Persona;
+
+@Repository
+public interface PersonaRepository extends JpaRepository<Persona, Long> {
 
 }
